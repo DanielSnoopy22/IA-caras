f2d0824aa543ff92ee5e27c83818961116caa4f4
DanielSnoopy22 <111645332+DanielSnoopy22@users.noreply.github.com>
2025-06-12 15:36:58 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Abra esta pagina en [https://danielsnoopy22.github.io/ia-caras/](https://danielsnoopy22.github.io/ia-caras/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/danielsnoopy22/ia-caras** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/danielsnoopy22/ia-caras** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,97 @@
+input.onButtonPressed(Button.B, function () {
+    music.stopAllSounds()
+    unlocked = false
+})
+let microbit_Choice2_type = ""
+let microbit_Choice = 0
+let SerialData = ""
+let unlocked = false
+unlocked = false
+let puntuación = 0
+music.setVolume(185)
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate9600
+)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(puntuación)
+    }
+})
+basic.forever(function () {
+    SerialData = serial.readString()
+})
+basic.forever(function () {
+    if (unlocked == false) {
+        if (SerialData == "CaraDaniel") {
+            basic.showIcon(IconNames.Yes)
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            unlocked = true
+        } else if (SerialData == "CaraEyenga") {
+            basic.showIcon(IconNames.Yes)
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            unlocked = true
+        } else if (SerialData == "Nada") {
+            unlocked = false
+            music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            basic.showIcon(IconNames.No)
+            basic.pause(200)
+            basic.showIcon(IconNames.Happy)
+        } else {
+            unlocked = false
+        }
+    } else if (unlocked == true) {
+        microbit_Choice = randint(1, 3)
+        SerialData = serial.readString()
+        basic.pause(1000)
+        basic.showLeds(`
+            # # # # #
+            . . . # .
+            . . # . .
+            # . . # .
+            . # # # .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            # # # . .
+            . . . # .
+            . . # . .
+            . # . . .
+            # # # # #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        music.play(music.tonePlayable(311, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        basic.clearScreen()
+        if (microbit_Choice == 1) {
+            microbit_Choice2_type = "Tijeras"
+        } else if (microbit_Choice == 2) {
+            microbit_Choice2_type = "Piedra"
+        } else if (microbit_Choice == 3) {
+            microbit_Choice2_type = "Papel"
+        }
+        if (SerialData == "Tijeras" && microbit_Choice2_type != "Tijeras") {
+            music.ringTone(880)
+            puntuación += 1
+        } else if (SerialData == "Papel" && microbit_Choice2_type != "Papel") {
+            music.ringTone(880)
+            puntuación += 1
+        } else if (SerialData == "Piedra" && microbit_Choice2_type != "Piedra") {
+            music.ringTone(880)
+            puntuación += 1
+        } else {
+            music.ringTone(147)
+            puntuación += -1
+        }
+    }
+    basic.pause(3000)
+})
